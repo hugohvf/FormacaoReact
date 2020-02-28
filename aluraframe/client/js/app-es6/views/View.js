b1477cd156ffc03965ccc3ecd0dcb0c5907c5c09
Hugo Fusinato <hugohvf@hotmail.com>
2020-02-28 16:40:20 -0300

    Curso finalizado

---
+++ aluraframe/client/js/app-es6/views/View.js
@@ -0,0 +1,16 @@
+export class View {
+
+    constructor(elemento) {
+        this._elemento = elemento;
+    }
+
+    template() {
+
+    throw new Error('O método template tem que ser implementado');
+
+    }
+
+    update (model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+}
